8656ebc7d9761d7c6c6b561a684ac58cf3db17c0
ibosh <atilla@pop-os.localdomain>
2022-02-28 20:52:30 +0100

    tried some stuff out

---
+++ portfolio/src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	import ButtonGroup from "./ButtonGroup.svelte"
+</script>
+<body>
+
+	<div>
+		<h1>Atilla Alsan</h1>
+		<p>Developer in medical informatics.</p>
+		<ButtonGroup />
+	</div>
+
+</body>
+
+<style>
+
+
+	body {
+		background-color: #6c7c98;
+	}
+
+	div {
+		text-align: center;
+		padding: 1em;
+		max-width: 300px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #FFEDCB;
+		font-size: 3em;
+		font-weight: 100;
+	}
+	
+</style>
---
+++ portfolio/src/ButtonGroup.svelte
@@ -0,0 +1,17 @@
+<div class="btn-group">
+    <button onclick="window.location.href = 'https://twitter.com/atilla_alsan'">Twitter</button>
+    <button onclick="window.location.href = 'https://instagram.com/atialsan'">Insta</button>
+    <button>Github</button>
+</div>
+
+<style>
+
+    button {
+        border-radius: 12px;
+        border: none;
+        background-color: #887456;
+        color: #FFEDCB;
+    }
+
+
+</style>
